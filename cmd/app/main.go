04bf2538000aac4b80d040e99280b7db79937828
reza-rayan <reza.hosseinzadeh@elanza.com>
2025-09-03 16:21:10 +0330

    Add: database connection

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/Reza-Rayan/internal/config"
+	"github.com/Reza-Rayan/internal/db"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Load Configs
 	cfg := config.LoadConfig()
+	// Load Database
+	db.InitDB()
 
 	server := gin.Default()
 
--- internal/db/gorm_db.go
+++ internal/db/gorm_db.go
@@ -1,1 +1,33 @@
 package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Reza-Rayan/internal/config"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	cfg := config.LoadConfig()
+
+	var err error
+	DB, err = gorm.Open(sqlite.Open(cfg.Database.Path), &gorm.Config{})
+	if err != nil {
+		panic(fmt.Sprintf("Could not connect to db: %v", err))
+	}
+
+	// AutoMigrate tables
+	err = DB.AutoMigrate(
+	//	Add Models HERE
+	)
+	if err != nil {
+		panic(fmt.Sprintf("Migration failed: %v", err))
+	}
+
+	log.Printf("✅ Connected to SQLite database: %s", cfg.Database.Path)
+}
